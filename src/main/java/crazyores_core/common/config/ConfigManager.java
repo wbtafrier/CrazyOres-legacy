25ed60b7f4ee622f99df7f94478bfb502657249a
Andy608 <AndrewR608@gmail.com>
2014-09-25 19:08:19 -0400

    Added comments to ConfigManager.

--- src/main/java/crazyores_core/common/config/ConfigManager.java
+++ src/main/java/crazyores_core/common/config/ConfigManager.java
@@ -1,10 +1,11 @@
 package crazyores_core.common.config;
-
 import net.minecraftforge.common.config.Configuration;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-
+/**
+ * ConfigManager manages all of CrazyOres Core's configuration files.
+ * @author Andy608 and ISQUISHALL
+ */
 public class ConfigManager {
-	
 	public static void writeFiles(FMLPreInitializationEvent event) {
 		Configuration configFile = new Configuration(event.getSuggestedConfigurationFile());
 	}
