4ce4405fdcea9e1b47f2dae7c2778d3c2fc43243
ISQUISHALL <wbtafrier@gmail.com>
2014-09-27 12:57:15 -0400

    Finished configuration file localization.

--- src/main/java/crazyores_core/common/config/ConfigManager.java
+++ src/main/java/crazyores_core/common/config/ConfigManager.java
@@ -1,6 +1,7 @@
 package crazyores_core.common.config;
 import org.apache.logging.log4j.Level;
 
+import net.minecraft.util.StatCollector;
 import net.minecraftforge.common.config.Configuration;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import crazyores_core.common.core.COLogger;
@@ -10,7 +11,7 @@ import crazyores_core.common.core.COLogger;
  */
 public class ConfigManager {
 	
-	private static final String WORLD_GEN_CATEGORY = "WORLD_GENERATION";
+	private static final String WORLD_GEN_CATEGORY = StatCollector.translateToLocal("config.category.worldGeneration");
 	
 	public static void writeFiles(FMLPreInitializationEvent event) {
 		// Creates main CO Core Configuration file.
@@ -18,28 +19,26 @@ public class ConfigManager {
 		COLogger.write(Level.INFO, "Loading CrazyOres Core main configuration file...");
 		configFile.load();
 		
-		//TODO: Add comments "When set to true, all CO ores will be generated in new chunks. Set to false to stop CO ores from spawning in new chunks."
-		
 		/** Ore Generation **/	
-		CoreSettings.generateOres = configFile.get(WORLD_GEN_CATEGORY, "GenerateOres", true).getBoolean(true);
-		CoreSettings.copperGen = configFile.get(WORLD_GEN_CATEGORY, "GenerateCopperOre", true).getBoolean(true);
-		CoreSettings.sapphireGen = configFile.get(WORLD_GEN_CATEGORY, "GenerateSapphireOre", true).getBoolean(true);
-		CoreSettings.adamiteGen = configFile.get(WORLD_GEN_CATEGORY, "GenerateAdamiteOre", true).getBoolean(true);
-		CoreSettings.meteoriteGen = configFile.get(WORLD_GEN_CATEGORY, "GenerateMeteoriteOre", true).getBoolean(true);
-		CoreSettings.rubyGen = configFile.get(WORLD_GEN_CATEGORY, "GenerateRubyOre", true).getBoolean(true);
-		CoreSettings.foolsRubyGen = configFile.get(WORLD_GEN_CATEGORY, "GenerateFoolsRubyOre", true).getBoolean(true);
-		CoreSettings.zectiumGen = configFile.get(WORLD_GEN_CATEGORY, "GenerateZectiumOre", true).getBoolean(true);
-		CoreSettings.tapaziteGen = configFile.get(WORLD_GEN_CATEGORY, "GenerateTapaziteOre", true).getBoolean(true);
-		CoreSettings.crystalGen = configFile.get(WORLD_GEN_CATEGORY, "GenerateCrystalOre", true).getBoolean(true);
-		CoreSettings.osmoniumGen = configFile.get(WORLD_GEN_CATEGORY, "GenerateOsmoniumOre", true).getBoolean(true);
-		CoreSettings.starconiumGen = configFile.get(WORLD_GEN_CATEGORY, "GenerateStarconiumOre", true).getBoolean(true);
-		CoreSettings.demoniteGen = configFile.get(WORLD_GEN_CATEGORY, "GenerateDemoniteStone", true).getBoolean(true);
-		CoreSettings.experiumGen = configFile.get(WORLD_GEN_CATEGORY, "GenerateExperiumOre", true).getBoolean(true);
-		CoreSettings.invisiumGen = configFile.get(WORLD_GEN_CATEGORY, "GenerateInvisiumOre", true).getBoolean(true);
-		CoreSettings.darkStoneGen = configFile.get(WORLD_GEN_CATEGORY, "GenerateDarkStone", true).getBoolean(true);
-		CoreSettings.luminiteGen = configFile.get(WORLD_GEN_CATEGORY, "GenerateLuminiteOre", true).getBoolean(true);
-		CoreSettings.enderGen = configFile.get(WORLD_GEN_CATEGORY, "GenerateEnderOre", true).getBoolean(true);
-		CoreSettings.ultraGen = configFile.get(WORLD_GEN_CATEGORY, "GenerateUltraOre", true).getBoolean(true);
+		CoreSettings.generateOres = configFile.get(WORLD_GEN_CATEGORY, StatCollector.translateToLocal("config.node.generateAllOres"), true, StatCollector.translateToLocal("config.comment.generateAllOres")).getBoolean(true);
+		CoreSettings.copperGen = configFile.get(WORLD_GEN_CATEGORY, StatCollector.translateToLocal("config.node.generateCopperOre"), true, StatCollector.translateToLocal("config.comment.generateCopperOre")).getBoolean(true);
+		CoreSettings.sapphireGen = configFile.get(WORLD_GEN_CATEGORY, StatCollector.translateToLocal("config.node.generateSapphireOre"), true, StatCollector.translateToLocal("config.comment.generateSapphireOre")).getBoolean(true);
+		CoreSettings.adamiteGen = configFile.get(WORLD_GEN_CATEGORY, StatCollector.translateToLocal("config.node.generateAdamiteOre"), true, StatCollector.translateToLocal("config.comment.generateAdamiteOre")).getBoolean(true);
+		CoreSettings.meteoriteGen = configFile.get(WORLD_GEN_CATEGORY, StatCollector.translateToLocal("config.node.generateMeteoriteOre"), true, StatCollector.translateToLocal("config.comment.generateMeteoriteOre")).getBoolean(true);
+		CoreSettings.rubyGen = configFile.get(WORLD_GEN_CATEGORY, StatCollector.translateToLocal("config.node.generateRubyOre"), true, StatCollector.translateToLocal("config.comment.generateRubyOre")).getBoolean(true);
+		CoreSettings.foolsRubyGen = configFile.get(WORLD_GEN_CATEGORY, StatCollector.translateToLocal("config.node.generateFoolsRubyOre"), true, StatCollector.translateToLocal("config.comment.generateFoolsRubyOre")).getBoolean(true);
+		CoreSettings.zectiumGen = configFile.get(WORLD_GEN_CATEGORY, StatCollector.translateToLocal("config.node.generateZectiumOre"), true, StatCollector.translateToLocal("config.comment.generateZectiumOre")).getBoolean(true);
+		CoreSettings.tapaziteGen = configFile.get(WORLD_GEN_CATEGORY, StatCollector.translateToLocal("config.node.generateTapaziteOre"), true, StatCollector.translateToLocal("config.comment.generateTapaziteOre")).getBoolean(true);
+		CoreSettings.crystalGen = configFile.get(WORLD_GEN_CATEGORY, StatCollector.translateToLocal("config.node.generateCrystalOre"), true, StatCollector.translateToLocal("config.comment.generateCrystalOre")).getBoolean(true);
+		CoreSettings.osmoniumGen = configFile.get(WORLD_GEN_CATEGORY, StatCollector.translateToLocal("config.node.generateOsmoniumOre"), true, StatCollector.translateToLocal("config.comment.generateOsmoniumOre")).getBoolean(true);
+		CoreSettings.starconiumGen = configFile.get(WORLD_GEN_CATEGORY, StatCollector.translateToLocal("config.node.generateStarconiumOre"), true, StatCollector.translateToLocal("config.comment.generateStarconiumOre")).getBoolean(true);
+		CoreSettings.demoniteGen = configFile.get(WORLD_GEN_CATEGORY, StatCollector.translateToLocal("config.node.generateDemoniteStone"), true, StatCollector.translateToLocal("config.comment.generateDemoniteStone")).getBoolean(true);
+		CoreSettings.experiumGen = configFile.get(WORLD_GEN_CATEGORY, StatCollector.translateToLocal("config.node.generateExperiumOre"), true, StatCollector.translateToLocal("config.comment.generateExperiumOre")).getBoolean(true);
+		CoreSettings.invisiumGen = configFile.get(WORLD_GEN_CATEGORY, StatCollector.translateToLocal("config.node.generateInvisiumOre"), true, StatCollector.translateToLocal("config.comment.generateInvisiumOre")).getBoolean(true);
+		CoreSettings.darkStoneGen = configFile.get(WORLD_GEN_CATEGORY, StatCollector.translateToLocal("config.node.generateDarkStone"), true, StatCollector.translateToLocal("config.comment.generateDarkStone")).getBoolean(true);
+		CoreSettings.luminiteGen = configFile.get(WORLD_GEN_CATEGORY, StatCollector.translateToLocal("config.node.generateLuminiteOre"), true, StatCollector.translateToLocal("config.comment.generateLuminiteOre")).getBoolean(true);
+		CoreSettings.enderGen = configFile.get(WORLD_GEN_CATEGORY, StatCollector.translateToLocal("config.node.generateEnderOre"), true, StatCollector.translateToLocal("config.comment.generateEnderOre")).getBoolean(true);
+		CoreSettings.ultraGen = configFile.get(WORLD_GEN_CATEGORY, StatCollector.translateToLocal("config.node.generateUltraOre"), true, StatCollector.translateToLocal("config.comment.generateUltraOre")).getBoolean(true);
 		
 		configFile.save();
 		COLogger.write(Level.INFO, "CrazyOres Core main configuration file successfully loaded!");
