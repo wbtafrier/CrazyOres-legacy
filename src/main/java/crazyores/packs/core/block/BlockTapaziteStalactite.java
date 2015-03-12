1579b36bc230b017dc24dedd81ca48125bdc8305
ISQUISHALL <wbtafrier@gmail.com>
2015-03-11 23:27:11 -0400

    Began work on Transporter

--- src/main/java/crazyores/packs/core/block/BlockTapaziteStalactite.java
+++ src/main/java/crazyores/packs/core/block/BlockTapaziteStalactite.java
@@ -1,24 +1,21 @@
 package crazyores.packs.core.block;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.passive.EntitySheep;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 import org.apache.logging.log4j.Level;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-
 import crazyores.manager.util.CrazyOresLogger;
-import net.minecraft.block.Block;
-import net.minecraft.block.Block.SoundType;
-import net.minecraft.client.Minecraft;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Blocks;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
 
 public class BlockTapaziteStalactite extends BlockTapaziteCrystal {
 	
