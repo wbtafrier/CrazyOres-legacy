eefab21339dbf1f9e08b5adc9c3e5b4562479511
ISQUISHALL <wbtafrier@gmail.com>
2015-09-01 01:33:20 -0400

    Fixed bug where dispensers would not treat Core Arrows or Eggs properly

--- src/main/java/crazyores/packs/core/entity/CoreEntityRegistry.java
+++ src/main/java/crazyores/packs/core/entity/CoreEntityRegistry.java
@@ -2,7 +2,22 @@ package crazyores.packs.core.entity;
 
 import java.util.Arrays;
 
+import net.minecraft.block.BlockDispenser;
+import net.minecraft.dispenser.BehaviorDefaultDispenseItem;
+import net.minecraft.dispenser.BehaviorProjectileDispense;
+import net.minecraft.dispenser.IBlockSource;
+import net.minecraft.dispenser.IPosition;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.entity.IProjectile;
+import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemMonsterPlacer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
 
 import com.google.common.base.Predicates;
@@ -32,6 +47,8 @@ import crazyores.packs.core.entity.tileentity.TileEntityDemoniteFurnace;
 import crazyores.packs.core.entity.tileentity.TileEntityLuminiteDisco;
 import crazyores.packs.core.entity.tileentity.TileEntityReinforcedChest;
 import crazyores.packs.core.entity.tileentity.TileEntityTapaziteCrystal;
+import crazyores.packs.core.item.CoreItems;
+import crazyores.packs.core.item.CoreSpawnEgg;
 import crazyores.packs.core.main.CrazyOresCore;
 
 public class CoreEntityRegistry {
@@ -67,6 +84,8 @@ public class CoreEntityRegistry {
 		
 		BiomeGenBase[] allBiomes = FluentIterable.from(Arrays.asList(BiomeGenBase.getBiomeGenArray())).filter(Predicates.notNull()).toArray(BiomeGenBase.class);
 		EntityRegistry.addSpawn(EntityGreatWhiteShark.class, 1, 1, 1, EnumCreatureType.waterCreature, allBiomes);
+		
+		registerDispenserBehaviors();
 	}
 	
 	public static void registerCoreTileEntities() {
@@ -74,5 +93,140 @@ public class CoreEntityRegistry {
 		GameRegistry.registerTileEntity(TileEntityDemoniteFurnace.class, "demonite_furnace");
 		GameRegistry.registerTileEntity(TileEntityLuminiteDisco.class, "luminite_disco");
 		GameRegistry.registerTileEntity(TileEntityReinforcedChest.class, "reinforced_chest");
+	}
+	
+	public static void registerDispenserBehaviors() {
+		BlockDispenser.dispenseBehaviorRegistry.putObject(CoreItems.flamingArrow, new BehaviorProjectileDispense()
+        {
+            /**
+             * Return the projectile entity spawned by this dispense behavior.
+             */
+            protected IProjectile getProjectileEntity(World world, IPosition position)
+            {
+                EntityFlamingArrow entityarrow = new EntityFlamingArrow(world, position.getX(), position.getY(), position.getZ());
+                entityarrow.canBePickedUp = 1;
+                return entityarrow;
+            }
+        });
+		
+		BlockDispenser.dispenseBehaviorRegistry.putObject(CoreItems.explosiveArrow, new BehaviorProjectileDispense()
+        {
+            /**
+             * Return the projectile entity spawned by this dispense behavior.
+             */
+            protected IProjectile getProjectileEntity(World world, IPosition position)
+            {
+                EntityExplosiveArrow entityarrow = new EntityExplosiveArrow(world, position.getX(), position.getY(), position.getZ());
+                entityarrow.canBePickedUp = 1;
+                return entityarrow;
+            }
+        });
+		
+		BlockDispenser.dispenseBehaviorRegistry.putObject(CoreItems.freezingArrow, new BehaviorProjectileDispense()
+        {
+            /**
+             * Return the projectile entity spawned by this dispense behavior.
+             */
+            protected IProjectile getProjectileEntity(World world, IPosition position)
+            {
+                EntityFreezingArrow entityarrow = new EntityFreezingArrow(world, position.getX(), position.getY(), position.getZ());
+                entityarrow.canBePickedUp = 1;
+                return entityarrow;
+            }
+        });
+		
+		BlockDispenser.dispenseBehaviorRegistry.putObject(CoreItems.lightningArrow, new BehaviorProjectileDispense()
+        {
+            /**
+             * Return the projectile entity spawned by this dispense behavior.
+             */
+            protected IProjectile getProjectileEntity(World world, IPosition position)
+            {
+                EntityLightningArrow entityarrow = new EntityLightningArrow(world, position.getX(), position.getY(), position.getZ());
+                entityarrow.canBePickedUp = 1;
+                return entityarrow;
+            }
+        });
+		
+		BlockDispenser.dispenseBehaviorRegistry.putObject(CoreItems.zectiumCore, new BehaviorProjectileDispense()
+        {
+            /**
+             * Return the projectile entity spawned by this dispense behavior.
+             */
+            protected IProjectile getProjectileEntity(World world, IPosition position)
+            {
+                EntityZectiumCore core = new EntityZectiumCore(world, position.getX(), position.getY(), position.getZ());
+                return core;
+            }
+        });
+		
+		BlockDispenser.dispenseBehaviorRegistry.putObject(CoreItems.greatWhiteSharkEgg, new BehaviorDefaultDispenseItem()
+        {
+            /**
+             * Dispense the specified stack, play the dispense sound and spawn particles.
+             */
+            public ItemStack dispenseStack(IBlockSource blockSource, ItemStack stack)
+            {
+                EnumFacing enumfacing = BlockDispenser.func_149937_b(blockSource.getBlockMetadata());
+                double d0 = blockSource.getX() + (double)((float)enumfacing.getFrontOffsetX() * 3.125F);
+                double d1 = (double)blockSource.getYInt();
+                double d2 = blockSource.getZ() + (double)((float)enumfacing.getFrontOffsetZ() * 3.125F);
+                Entity entity = ((CoreSpawnEgg)CoreItems.greatWhiteSharkEgg).spawnEntity(blockSource.getWorld(), d0, d1, d2);
+
+                if (entity instanceof EntityLivingBase && stack.hasDisplayName())
+                {
+                    ((EntityLiving)entity).setCustomNameTag(stack.getDisplayName());
+                }
+
+                stack.splitStack(1);
+                return stack;
+            }
+        });
+		
+		BlockDispenser.dispenseBehaviorRegistry.putObject(CoreItems.adamiteSharkEgg, new BehaviorDefaultDispenseItem()
+        {
+            /**
+             * Dispense the specified stack, play the dispense sound and spawn particles.
+             */
+            public ItemStack dispenseStack(IBlockSource blockSource, ItemStack stack)
+            {
+                EnumFacing enumfacing = BlockDispenser.func_149937_b(blockSource.getBlockMetadata());
+                double d0 = blockSource.getX() + (double)((float)enumfacing.getFrontOffsetX() * 3.125F);
+                double d1 = (double)blockSource.getYInt();
+                double d2 = blockSource.getZ() + (double)((float)enumfacing.getFrontOffsetZ() * 3.125F);
+                Entity entity = ((CoreSpawnEgg)CoreItems.adamiteSharkEgg).spawnEntity(blockSource.getWorld(), d0, d1, d2);
+
+                if (entity instanceof EntityLivingBase && stack.hasDisplayName())
+                {
+                    ((EntityLiving)entity).setCustomNameTag(stack.getDisplayName());
+                }
+
+                stack.splitStack(1);
+                return stack;
+            }
+        });
+		
+		BlockDispenser.dispenseBehaviorRegistry.putObject(CoreItems.zectiumProtectorEgg, new BehaviorDefaultDispenseItem()
+        {
+            /**
+             * Dispense the specified stack, play the dispense sound and spawn particles.
+             */
+            public ItemStack dispenseStack(IBlockSource blockSource, ItemStack stack)
+            {
+                EnumFacing enumfacing = BlockDispenser.func_149937_b(blockSource.getBlockMetadata());
+                double d0 = blockSource.getX() + (double)enumfacing.getFrontOffsetX();
+                double d1 = (double)((float)blockSource.getYInt() + 0.2F);
+                double d2 = blockSource.getZ() + (double)enumfacing.getFrontOffsetZ();
+                Entity entity = ((CoreSpawnEgg)CoreItems.zectiumProtectorEgg).spawnEntity(blockSource.getWorld(), d0, d1, d2);
+
+                if (entity instanceof EntityLivingBase && stack.hasDisplayName())
+                {
+                    ((EntityLiving)entity).setCustomNameTag(stack.getDisplayName());
+                }
+
+                stack.splitStack(1);
+                return stack;
+            }
+        });
 	}
 }
