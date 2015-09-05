59c7d643705e46b27dca85e9cd11428f01686696
Andy608 <AndrewR608@gmail.com>
2015-09-05 13:46:46 -0400

    Hopefully fixed breakblock event glitch.

--- src/main/java/crazyores/packs/core/event/CoreChatEvent.java
+++ src/main/java/crazyores/packs/core/event/CoreChatEvent.java
@@ -9,7 +9,12 @@ import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.event.world.BlockEvent.BreakEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import crazyores.manager.pack.COPackManager;
+import crazyores.packs.core.block.BlockOre;
+import crazyores.packs.core.block.BlockTapaziteCrystal;
+import crazyores.packs.core.block.CoreBlock;
 import crazyores.packs.core.block.CoreGlass;
+import crazyores.packs.core.block.CoreStairs;
+import crazyores.packs.core.item.CorePickaxe;
 
 public class CoreChatEvent {
 
@@ -17,16 +22,27 @@ public class CoreChatEvent {
 	public void onBadMineEvent(BreakEvent e) {
 		
 		EntityPlayer player = e.getPlayer();
-		if (player != null && !player.capabilities.isCreativeMode && player.getCurrentEquippedItem() != null && player.getCurrentEquippedItem().getItem() != null) {
+		if (player != null && !player.capabilities.isCreativeMode && player.getCurrentEquippedItem() != null && player.getCurrentEquippedItem().getItem() != null && e.block != null) {
 			Item item = player.getCurrentEquippedItem().getItem();
-			if (item instanceof ItemPickaxe) {
-				if (!(e.block instanceof CoreGlass)) {
-					if (item.getHarvestLevel(player.getCurrentEquippedItem(), "pickaxe") < e.block.getHarvestLevel(0)) {
+			if (item instanceof CorePickaxe || e.block instanceof CoreBlock || e.block instanceof BlockTapaziteCrystal || e.block instanceof CoreStairs) {
+				if (!(e.block instanceof CoreGlass) && e.block != null) {
+					if (item.getHarvestLevel(player.getCurrentEquippedItem(), "pickaxe") < e.block.getHarvestLevel(e.blockMetadata)) {
 						
 						String pickaxe = player.getCurrentEquippedItem().getDisplayName();
-						String block = (new ItemStack(e.block, 1, e.blockMetadata)).getDisplayName();
+						
+						ItemStack s = new ItemStack(e.block, 0, e.blockMetadata);
+						String block = null;
+						
+						if (s != null) {
+							block = (new ItemStack(e.block, 0, e.blockMetadata)).getDisplayName();
+						}
+						
+						if (block == null) {
+							block = "this block";
+						}
+						
 						player.addChatMessage(new ChatComponentText(EnumChatFormatting.AQUA + COPackManager.corePack.getFullPackName() + EnumChatFormatting.RESET + " Uh oh! " + isAorAn(true, pickaxe) + " " + pickaxe
-								+ " is unable to mine " + isAorAn(false, block) + " " + block + ". To learn more about the mining system, go to:"));
+								+ " is unable to mine " + isAorAn(false, block) + " " + block + ". To learn more about the CrazyOres tier system, go to:"));
 						player.addChatMessage(new ChatComponentText("http://tinyurl.com/nvgxgya"));
 					}
 				}
